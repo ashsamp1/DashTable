569cc07d69c28c3c54575ed1a78924595a949470
Akash Mishra <46259169+akash19x@users.noreply.github.com>
2020-10-16 09:04:15 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,14 +2,14 @@ import dash
 import dash_table
 import pandas as pd
 
-df = pd.read_csv('https://api.covid19india.org/csv/latest/state_wise.csv')
+df = pd.read_csv('https://api.covid19india.org/csv/latest/state_wise.csv') # covid 19 dataset state wise.
 bootstrap = "https://stackpath.bootstrapcdn.com/bootswatch/4.5.2/solar/bootstrap.min.css"
 app = dash.Dash(__name__, external_stylesheets=[bootstrap])
 
 app.layout = dash_table.DataTable(
     id='table',
     columns=[{"name": i, "id": i} for i in df.columns],
-    data=df.to_dict('records'),
+    data=df.to_dict('records')
 )
 
 if __name__ == '__main__':
